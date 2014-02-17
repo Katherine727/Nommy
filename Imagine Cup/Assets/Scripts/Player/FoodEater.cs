08da0a8f11adc2c801c03cb10017200132ff34b4
Szymon Licau <szymi142@gmail.com>
2014-02-17 17:14:21 +0100

    Wstepne przygotowania do mechanizmu pozerania pokarmow

---
+++ Imagine Cup/Assets/Scripts/Player/FoodEater.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FoodEater : MonoBehaviour {
+
+	public float SatiationRate = 0f;//stan nasycenia naszego pozeracza
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	//Po kolizji z jakims triggerem(triggerami beda np smakolyki)
+	void OnTriggerEnter2D(Collider2D c)
+	{
+		if (c.gameObject.layer == LayerMask.NameToLayer("Food")) //jesli smakolyk
+		{
+			Destroy(c.gameObject); //to niszcz go
+
+			//KOD DAJACY POWER_UP TO_DO
+		}
+	}
+}
