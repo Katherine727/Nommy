54f6a6954b1934814e87fa4787c524e8dfcd877c
Szymon Licau <szymi142@gmail.com>
2014-03-01 18:23:41 +0100

    Dorobiony splash gry i dalsze ogarnianie folderów

---
+++ Imagine Cup/Assets/Scripts/Intro/ReactToInput.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ReactToInput : MonoBehaviour {
+
+	private SplashScreen ss = null;
+
+	public Texture2D SplashAfter;
+	AudioSource asrc;
+	bool playedOnce=false;
+
+	// Update is called once per frame
+	void Update () {		
+		if(ss == null || asrc == null) {
+			ss = Camera.current.GetComponent<SplashScreen>();
+			asrc = this.gameObject.GetComponent<AudioSource>();
+		}
+		else {
+			if(ss.CurrentStatus == SplashScreen.FadeStatus.FadeOut) {
+				ss.splashLogo = SplashAfter;
+				if(!playedOnce) {
+					asrc.Play ();
+					playedOnce=true;
+				}
+			}
+		}
+	}
+}
