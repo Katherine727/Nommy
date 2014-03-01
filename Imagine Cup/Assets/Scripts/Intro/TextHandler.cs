46feaa7bc408c4eaef6e7bfb1f3cf1caf102c436
Szymon Licau <szymi142@gmail.com>
2014-03-01 19:19:29 +0100

    Wersja ostateczna splasha z zachętą do wciskania klawisza

---
+++ Imagine Cup/Assets/Scripts/Intro/TextHandler.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextHandler : MonoBehaviour {
+
+	Animator a;
+	SplashScreen ss;
+	bool playedOnce = false;
+
+	void Start() {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if( a == null || ss == null) {
+			a = this.gameObject.GetComponent<Animator>();
+			ss = Camera.current.GetComponent<SplashScreen>();
+		} else {
+			if(!playedOnce) {
+				if(ss.CurrentStatus == SplashScreen.FadeStatus.FadeOut) {
+					a.Play(Animator.StringToHash("Fade out"));
+					playedOnce=true;
+				}
+			}
+		}
+	}
+}
