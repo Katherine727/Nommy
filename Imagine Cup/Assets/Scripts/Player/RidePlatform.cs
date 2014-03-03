5c3e6b13af9aebb5d2d70991bf057c0e70090c8c
Szymon Licau <szymi142@gmail.com>
2014-03-03 02:38:51 +0100

    Platformy working. No sleep mode.

--- Imagine Cup/Assets/Scripts/Player/RidePlatform.cs
+++ Imagine Cup/Assets/Scripts/Player/RidePlatform.cs
@@ -3,11 +3,13 @@ using System.Collections;
 
 public class RidePlatform : MonoBehaviour {
 
-    void OnTriggerExit2D(Collider2D c) {
-        if (c.gameObject.tag == "Platforms") {
-            transform.parent = null;
-        }
-    }
+    void Update() {
+		if(this.transform.parent != null) {
+			if(!this.collider2D.renderer.bounds.Intersects(this.transform.parent.collider2D.renderer.bounds))
+				this.transform.parent=null;
+		}
+	}
+
     void OnTriggerEnter2D(Collider2D c) {
         if (c.gameObject.tag == "Platforms") {
             transform.parent = c.transform;
