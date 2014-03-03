8a4a75e23b0ea0133107fda0d0b56af7ceee035c
Łukasz Kiełczykowski <kielczykowski.lukasz@gmail.com>
2014-03-03 21:32:18 +0100

    fix nazw - usingmultiplayer hyh

---
+++ Imagine Cup/Assets/Scripts/Slots/UsePowerWithSlot.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UsePowerWithSlot : MonoBehaviour {
+
+    public GameObject slotManagerObject;
+
+    private SlotManager slotManager;
+	void Start () {
+        slotManager = slotManagerObject.GetComponent<SlotManager>();
+        if (slotManager == null) {
+            Debug.LogError("SlotManager component is required in given object.");
+        }
+        //TODO!
+        //wczytanie komponentu zwiazanego z moco
+	}
+	
+	void Update () {
+        if (!slotManager.ActivatedSlot.IsUsing) {
+            if (Input.GetKeyDown(KeyCode.Alpha1)) {
+                slotManager.ActivateSlot(0);
+            } else if (Input.GetKeyDown(KeyCode.Alpha2)) {
+                slotManager.ActivateSlot(1);
+            } else if (Input.GetKeyDown(KeyCode.Alpha3)) {
+                slotManager.ActivateSlot(2);
+            } else if (Input.GetKeyDown(KeyCode.Alpha4)) {
+                slotManager.ActivateSlot(3);
+            } else if (Input.GetKeyDown(KeyCode.Q)) {
+                slotManager.ActivatePrev();
+            } else if (Input.GetKeyDown(KeyCode.E)) {
+                slotManager.ActivateNext();
+            }
+        }
+
+        if (slotManager.Slots.Count > 0) {
+            if (Input.GetMouseButton(0)) {
+                slotManager.Slots[slotManager.IndexOfActivatedSlot].IsUsing = true;
+            } else {
+                slotManager.Slots[slotManager.IndexOfActivatedSlot].IsUsing = false;
+            }
+        }
+
+        if (slotManager.ActivatedSlot.IsUsing) {
+            //Use brand you pała => sugarbrick C: => uzyj slotManager.ActivatedSlot.Power :)
+        }
+
+	}
+}
