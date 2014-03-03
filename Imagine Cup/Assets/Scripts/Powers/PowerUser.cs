1a579d116f52a43b73490f48d1dc4ca972a763f2
Szymon Licau <szymi142@gmail.com>
2014-03-03 21:33:18 +0100

    Fabryka Powerów, pattern Polecenia i jego implementacja na przykładzie. Niestety Timer slota nie reaguje na nasze niecne używanie mocy.

---
+++ Imagine Cup/Assets/Scripts/Powers/PowerUser.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using AssemblyCSharp;
+
+public class PowerUser : MonoBehaviour {
+	private PowerCommandFactory pcf;
+	private SlotManager sm;
+
+	// Use this for initialization
+	void Start () {
+		pcf = new PowerCommandFactory(this.gameObject);	
+	}
+
+	void Awake() {
+		sm = FindObjectOfType<SlotManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(sm.Slots.Count>0) {
+			if(Input.GetKey(KeyCode.Space)){ //The "Use Power" Key
+				UsePower(sm.ActivatedSlot.Power);
+				sm.Slots[sm.IndexOfActivatedSlot].IsUsing=true;
+			} else {
+				sm.Slots[sm.IndexOfActivatedSlot].IsUsing=false;
+			}
+		}
+		Debug.Log(sm.Slots[sm.IndexOfActivatedSlot].IsActive);
+	}
+
+	void UsePower(PowerEnum PowerType) {
+		ICommand PowerCommand = pcf.CreatePowerCommand(PowerType);
+
+		PowerCommand.Execute();
+	}
+}
