0ed46729d0b5702a312599d5729e57f77276c477
Szymon Licau <szymi142@gmail.com>
2014-03-11 14:24:26 +0100

    Teleportery, prefab teleportera i scena testujaca je(nie ma juz spadania w pustke!)

---
+++ Imagine Cup/Assets/Scripts/General/TeleportObjectToLocation.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using Assets.Utils;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class TeleportObjectToLocation : MonoBehaviour {
+
+	public Transform TeleportLocation;
+
+	void OnCollisionEnter2D(Collision2D c) {
+		c.transform.position = TeleportLocation.position;
+	}
+}
