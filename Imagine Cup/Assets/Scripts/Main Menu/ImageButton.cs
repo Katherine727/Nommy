a0ea79f20952fc094e121ae00cd634d33475cf4d
Szymon Licau <szymi142@gmail.com>
2014-03-12 21:02:33 +0100

    Zrobienie by menu "działało". Shitowo bardzo ale działa. Potem sie zrefactoruje

--- Imagine Cup/Assets/Scripts/Main Menu/ImageButton.cs
+++ Imagine Cup/Assets/Scripts/Main Menu/ImageButton.cs
@@ -5,13 +5,19 @@ using System.Collections;
 [RequireComponent(typeof(SpriteRenderer))]
 [RequireComponent(typeof(AudioSource))]
 public class ImageButton : MonoBehaviour {
-	
+
+	public enum OnClickActionType { NewGame=0, Options, Exit, Credits, Default };
+
 	public Sprite HoverSprite;
 	public Sprite OnClickSprite;
+	public OnClickActionType ActionType;
 
 	private SpriteRenderer sr;
 	private AudioSource asrc;
 	private Sprite defaultSprite;
+	private float delay=0.8f;
+	private float currentDelay=0;
+	private bool launchingLevel = false;
 
 	// Use this for initialization
 	void Start () {
@@ -35,6 +41,34 @@ public class ImageButton : MonoBehaviour {
 		if(Input.GetMouseButtonDown(0)){
 			sr.sprite = OnClickSprite;
 			asrc.Play();
+
+			switch(ActionType) {
+				case OnClickActionType.NewGame:
+					launchingLevel=true;
+					break;
+
+				case OnClickActionType.Exit:
+					Application.Quit();
+					break;
+
+				case OnClickActionType.Options:
+					break;
+				case OnClickActionType.Credits:
+					break;
+				case OnClickActionType.Default:
+					break;
+				default:
+					break;
+			}
+		}
+	}
+
+	void Update() {
+		if(launchingLevel) {
+			currentDelay += Time.deltaTime;
+			if(currentDelay >= delay) {
+				Application.LoadLevel("Tutorial_MAIN");
+			}
 		}
 	}
 }
